b29bc928f7b47e22b239dee573b53623e4798617
Mahnaz <85938121+mahnazmohammadzamani@users.noreply.github.com>
2021-11-02 16:50:03 +0100

    Add files via upload

---
+++ filter_my_vector.R
@@ -0,0 +1,7 @@
+name <- "Omid"
+liuid <- "nnnnn000"
+filter_my_vector <- function(x,leq)
+{ifelse(x>=leq,NA, x)}
+filter_my_vector(x = c(2, 9, 2, 4, 102), leq = 4)
+mark_my_assignment(tasks='filter_my_vector')
+
